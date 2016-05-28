8f51e7a16c71c87a031bfb14f8b6b0f48f63a49c
Héctor Barreras Almarcha <hbaelx@gmail.com>
2016-05-28 20:17:13 +0200

    Fixed some stuff

--- nosbot/NewsDataSource.py
+++ nosbot/NewsDataSource.py
@@ -1,5 +1,5 @@
 import requests
-import NewsItem
+import nosbot.NewsItem
 from datetime import datetime, timedelta
 from asyncio import Lock
 
@@ -15,17 +15,13 @@ class NewsDataSource:
     def get_top_news(self, amount, topic_list=[]):
         print("Getting the top {} news items".format(amount))
         if datetime.now() - self.last_sort_time > timedelta(minutes=5):
-            yield from self.sort_lock
-            print("Sorting the news")
             self.cached_news.sort(key=lambda news_item: news_item.interesting_counter, reverse=True)
-            self.sort_lock.release()
 
         if len(topic_list) == 0:
-            print("No topic list set")
-            print("We have {} cached items".format(len(self.cached_news)))
             return self.cached_news[:amount]
         else:
-            return (item for item in self.cached_news if NewsDataSource._news_item_contains_any_topic(item, topic_list))[:amount]
+            #No amount so far, sorry not sorry
+            return (item for item in self.cached_news if NewsDataSource._news_item_contains_any_topic(item, topic_list))#[:amount]
 
     @staticmethod
     def _news_item_contains_any_topic(newsItem, topics_list):
@@ -40,7 +36,7 @@ class NewsDataSource:
 
         json = r.json()
         for entry in json['items']:
-            self.cached_news.append(NewsItem.NewsItem(entry))
+            self.cached_news.append(nosbot.NewsItem.NewsItem(entry))
 
 if __name__ == '__main__':
     s = NewsDataSource('http://s.nos.nl/extern/nieuws.json')
